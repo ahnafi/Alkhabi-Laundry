427084b4af91854ed3866b6350c490c06343911a
ebiina <essay.mukti@mhs.unsoed.ac.id>
2025-06-01 16:44:29 +0700

    ganti nama alkhabi ajah

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,7 +7,7 @@
 <div class="flex justify-between h-16">
     <div class="flex">
         <div class="flex-shrink-0 flex items-center">
-            <span class="text-pink-600 font-bold text-2xl">PinkWash</span>
+            <span class="text-pink-600 font-bold text-2xl">Alkhabi</span>
         </div>
         <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
             <a href="#" class="border-pink-500 text-pink-600 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
